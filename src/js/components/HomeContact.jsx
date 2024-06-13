ce1ff4fc88561dc2012b9110b21d3f2c48d4ea5d
Mateusz Obirek <neutralnie7@gmail.com>
2024-06-13 19:57:03 +0200

    added all form styles

--- src/js/components/HomeContact.jsx
+++ src/js/components/HomeContact.jsx
@@ -6,26 +6,27 @@ const HomeContact = () => {
           <h3>Skontaktuj się z nami</h3>
           <div className="ornament" />
           <form>
-            <div>
-              <label>
+            <div className="form-name-box">
+              <label className="placeholder-text">
                 Wpisz swoje imię
                 <input type="text" name="name" placeholder="Krzysztof" />
               </label>
-              <label>
+              <label className="placeholder-text">
                 Wpisz swój email
                 <input type="email" name="email" placeholder="abc@xyz.pl" />
               </label>
             </div>
-            <label className="textarea-box">
-              Wpisz swoją wiadomość
-              <input
-                type="textarea"
-                name="msg"
-                placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\n eiusmod tempor incididunt ut labore et dolore magna aliqua.\n Ut enim ad minim veniam, quis nostrud exercitation ullamco\n laboris nisi ut aliquip ex ea commodo consequat."
-              />
-            </label>
+            <div className="textarea-box">
+              <label>
+                Wpisz swoją wiadomość
+                <textarea
+                  name="msg"
+                  placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+              </label>{" "}
+            </div>
 
-            <input type="submit" value="Wyślij" />
+            <input className="submit-btn" type="submit" value="Wyślij" />
           </form>
         </div>
         {/*<footer>*/}
