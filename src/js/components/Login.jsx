0233e4dccee98d26ced77202ca5a40df165fe9a4
Mateusz Obirek <neutralnie7@gmail.com>
2024-06-14 15:43:10 +0200

    added validation in login component

--- src/js/components/Login.jsx
+++ src/js/components/Login.jsx
@@ -1,21 +1,66 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [error, setError] = useState("");
+
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      );
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!validateEmail(email)) {
+      setError("error-email");
+    } else if (password.length < 6) {
+      setError("error-password");
+    } else {
+      setError("");
+      setEmail("");
+      setPassword("");
+    }
+  };
+
   return (
     <>
       <section className="login">
         <h2>Zaloguj się</h2>
         <div className="ornament" />
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className="login-window">
             <div className="login-box">
               <label>
                 Email
-                <input type="email" />
+                <input
+                  value={email}
+                  name={email}
+                  type="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {error.includes("error-email") ? (
+                  <p className="error-email">Podany email jest niepoprawny!</p>
+                ) : null}
               </label>
               <label>
                 Hasło
-                <input type="password" />
+                <input
+                  value={password}
+                  name={password}
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                {error.includes("error-password") ? (
+                  <p className="error-password">
+                    Podane hasło jest za krótkie!
+                  </p>
+                ) : null}
               </label>
             </div>
           </div>
@@ -23,7 +68,7 @@ const Login = () => {
             <Link to="/rejestracja">
               <button>Załóż konto</button>
             </Link>
-            <button>Zaloguj się</button>
+            <button type="submit">Zaloguj się</button>
           </div>
         </form>
       </section>
