aeef61c9c9b5691dd7da71ce61d86a3da50d59f6
Mateusz Obirek <neutralnie7@gmail.com>
2024-06-14 15:18:22 +0200

    added login layout

--- src/js/components/Login.jsx
+++ src/js/components/Login.jsx
@@ -1,4 +1,33 @@
+import { Link } from "react-router-dom";
+
 const Login = () => {
-  return;
+  return (
+    <>
+      <section className="login">
+        <h2>Zaloguj się</h2>
+        <div className="ornament" />
+        <form>
+          <div className="login-window">
+            <div className="login-box">
+              <label>
+                Email
+                <input type="email" />
+              </label>
+              <label>
+                Hasło
+                <input type="password" />
+              </label>
+            </div>
+          </div>
+          <div className="login-btns-box">
+            <Link to="/rejestracja">
+              <button>Załóż konto</button>
+            </Link>
+            <button>Zaloguj się</button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 export default Login;
