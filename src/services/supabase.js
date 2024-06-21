ee641c05a336bce79358129182d723ea5fd8258a
Mateusz Obirek <neutralnie7@gmail.com>
2024-06-21 09:17:27 +0200

    installed supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://kvinjqxuqtbaqcahhzkr.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2aW5qcXh1cXRiYXFjYWhoemtyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTg5NTM0MTYsImV4cCI6MjAzNDUyOTQxNn0.KTCw-oB5E0JhpMIb0BU7eRmTJ7owOynGutxWTLBzS94";
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
