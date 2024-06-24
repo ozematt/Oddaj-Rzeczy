60a9376e2d9683c6f4d73f2b23e17c52262ae674
Mateusz Obirek <neutralnie7@gmail.com>
2024-06-24 14:24:00 +0200

    added hamburger menu

--- src/js/components/Navbar.jsx
+++ src/js/components/Navbar.jsx
@@ -1,39 +1,88 @@
 import { Link as ScrollLink } from "react-scroll";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  ////DATA
+  const [classToggle, setClassToggle] = useState(false);
+  ////LOGIC
+  const handleHamburgerClick = () => {
+    setClassToggle(!classToggle);
+  };
+
+  ///UI
   return (
-    <nav className="navbar">
-      <ul>
-        <li>
-          <ScrollLink to="start" smooth={true} duration={500}>
-            <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-              Start
-            </Link>
-          </ScrollLink>
-        </li>
-        <li>
-          <ScrollLink to="info" smooth={true} duration={500}>
-            O co chodzi?
-          </ScrollLink>
-        </li>
-        <li>
-          <ScrollLink to="aboutUs" smooth={true} duration={500}>
-            O nas
-          </ScrollLink>
-        </li>
-        <li>
-          <ScrollLink to="fundations" smooth={true} duration={500}>
-            Fundacja i organizacje
-          </ScrollLink>
-        </li>
-        <li>
-          <ScrollLink to="contact" smooth={true} duration={500}>
-            Kontakt
-          </ScrollLink>
-        </li>
-      </ul>
-    </nav>
+    <>
+      <div className="hamburger" onClick={handleHamburgerClick}>
+        <div
+          className={classToggle ? "hamburger-menu" : "hamburger-menu hidden"}
+        >
+          <ul>
+            <li>
+              <ScrollLink to="start" smooth={true} duration={500}>
+                <Link
+                  to="/"
+                  style={{ textDecoration: "none", color: "inherit" }}
+                >
+                  Start
+                </Link>
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="info" smooth={true} duration={500}>
+                O co chodzi?
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="aboutUs" smooth={true} duration={500}>
+                O nas
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="fundations" smooth={true} duration={500}>
+                Fundacja i organizacje
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="contact" smooth={true} duration={500}>
+                Kontakt
+              </ScrollLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <nav className="navbar">
+        <ul>
+          <li>
+            <ScrollLink to="start" smooth={true} duration={500}>
+              <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+                Start
+              </Link>
+            </ScrollLink>
+          </li>
+          <li>
+            <ScrollLink to="info" smooth={true} duration={500}>
+              O co chodzi?
+            </ScrollLink>
+          </li>
+          <li>
+            <ScrollLink to="aboutUs" smooth={true} duration={500}>
+              O nas
+            </ScrollLink>
+          </li>
+          <li>
+            <ScrollLink to="fundations" smooth={true} duration={500}>
+              Fundacja i organizacje
+            </ScrollLink>
+          </li>
+          <li>
+            <ScrollLink to="contact" smooth={true} duration={500}>
+              Kontakt
+            </ScrollLink>
+          </li>
+        </ul>
+      </nav>
+    </>
   );
 };
 export default Navbar;
