7dc019d91b37eaf001b079035df08095deed1e07
Mateusz Obirek <neutralnie7@gmail.com>
2024-10-28 08:25:52 +0100

    added supabase on hold

--- src/js/components/Register.jsx
+++ src/js/components/Register.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { supabase } from "../../services/supabase.js";
+// import { supabase } from "../../services/supabase.js";
 import { useNavigate } from "react-router-dom";
 import { useStoreState, useStoreActions } from "easy-peasy";
 
