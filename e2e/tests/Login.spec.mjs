3ba74046ea0288efe53cb02bd030ce760098dd98
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-11 13:38:06 +0100

    deleted tests

--- e2e/tests/Login.spec.mjs
+++ e2e/tests/Login.spec.mjs
@@ -1,38 +1,3 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("Login flow", () => {
-  test("Go to login page, submit form with valid data and verify redirection", async ({
-    page,
-  }) => {
-    await page.goto("/");
-
-    const loginButton = page.getByRole("button", { name: "Zaloguj" });
-    await loginButton.click();
-
-    const emailInput = page.getByRole("textbox", { name: "Email" });
-    const passwordInput = page.getByRole("textbox", { name: "Hasło" });
-
-    await emailInput.fill("neutralnie7@gmail.com");
-    await passwordInput.fill("111111");
-    await page.getByRole("button", { name: "Zaloguj się" }).click();
-
-    await expect(page.getByText("Cześć, neutralnie7@gmail.com")).toBeVisible();
-  });
-  test("Go to login page, submit form with invalid data check error display", async ({
-    page,
-  }) => {
-    await page.goto("/");
-
-    const loginButton = page.getByRole("button", { name: "Zaloguj" });
-    await loginButton.click();
-
-    const emailInput = page.getByRole("textbox", { name: "Email" });
-    const passwordInput = page.getByRole("textbox", { name: "Hasło" });
-
-    await emailInput.fill("invalid@Email");
-    await passwordInput.fill("111");
-    await page.getByRole("button", { name: "Zaloguj się" }).click();
-
-    await expect(page.getByText("Podany email jest niepoprawny")).toBeVisible();
-  });
-});
+test.describe("Login flow", () => {});
