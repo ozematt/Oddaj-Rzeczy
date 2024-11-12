f335e118aaf834ae10c12b03d641246d25aa6591
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-12 12:52:47 +0100

    test CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: "http://127.0.0.1:5173",
+    baseURL: "http://0.0.0.0:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -72,8 +72,8 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "npm run dev",
-    url: "http://127.0.0.1:5173",
+    url: "http://0.0.0.0:5173",
     reuseExistingServer: !process.env.CI,
-    timeout: 130 * 1000,
+    timeout: 120 * 1000,
   },
 });
