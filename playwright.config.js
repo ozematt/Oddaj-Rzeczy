9dc7b5f1af0cf656e58136be8d2ccbd3b5f73f36
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-13 10:05:45 +0100

    test CI

--- playwright.config.js
+++ playwright.config.js
@@ -74,7 +74,7 @@ export default defineConfig({
     command: "npm run start",
     url: "http://127.0.0.1:5173",
     reuseExistingServer: !process.env.CI,
-    timeout: 130 * 1000,
+    timeout: 120 * 1000,
     stdout: "pipe",
     stderr: "pipe",
   },
