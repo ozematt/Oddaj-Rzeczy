d7391f74ed3f3654f1a920b62d6a6788d28176d9
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-13 10:05:15 +0100

    updated package

--- playwright.config.js
+++ playwright.config.js
@@ -71,7 +71,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run ci:start",
+    command: "npm run start",
     url: "http://127.0.0.1:5173",
     reuseExistingServer: !process.env.CI,
     timeout: 130 * 1000,
