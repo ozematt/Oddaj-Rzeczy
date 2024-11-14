567ee0ab89900c55b7322da43acb349bd423f12d
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-14 09:36:04 +0100

    test CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -73,6 +73,6 @@ export default defineConfig({
   webServer: {
     command: "npm run start",
     url: "http://127.0.0.1:5173",
-    reuseExistingServer: true,
+    reuseExistingServer: !process.env.CI,
   },
 });
