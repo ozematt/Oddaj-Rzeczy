1df810031c60f8e05231f473b4ff3982e127e3d5
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-14 10:08:21 +0100

    teeeest

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,6 +29,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
+    headless: true,
   },
 
   /* Configure projects for major browsers */
@@ -72,10 +73,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: "npm run start",
-    url: "http://localhost:5173",
-    reuseExistingServer: false,
-    timeout: 120 * 1000,
-  },
+  // webServer: {
+  //   command: "npm run start",
+  //   url: "http://localhost:5173",
+  //   reuseExistingServer: false,
+  //   timeout: 120 * 1000,
+  // },
 });
