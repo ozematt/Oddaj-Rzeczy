daf373e1fabbcd900aa80fb20ae97207850a1c37
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-15 14:21:06 +0100

    added test

--- e2e/Contact.spec.ts
+++ e2e/Contact.spec.ts
@@ -44,4 +44,29 @@ test.describe("Contact submit flow", () => {
       page.getByText("Wiadomość musi mieć conajmniej 120 znaków!")
     ).toBeVisible();
   });
+  test("should display error messages after submit contact form with invalid data ", async ({
+    page,
+  }) => {
+    await page.goto("/");
+    const contactButton = page
+      .getByRole("navigation")
+      .locator("li")
+      .filter({ hasText: "Kontakt" });
+    await contactButton.click();
+    await page.getByPlaceholder("Krzysztof").fill("111");
+    await page.getByPlaceholder("abc@xyz.pl").fill("invalid@Email");
+    await page.getByPlaceholder("Lorem ipsum dolor sit amet,").fill("12345");
+
+    await page.getByRole("button", { name: "Wyślij" }).click();
+
+    await expect(
+      page.getByText("Podane imię jest nieprawidłowe!")
+    ).toBeVisible();
+    await expect(
+      page.getByText("Podany email jest nieprawidłowy!")
+    ).toBeVisible();
+    await expect(
+      page.getByText("Wiadomość musi mieć conajmniej 120 znaków!")
+    ).toBeVisible();
+  });
 });
