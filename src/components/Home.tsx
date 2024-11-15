4f337117e802a4542994f8b5ced937623479260d
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-15 14:24:57 +0100

    edited imports

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,10 @@
+import { HomeHeader } from "./HomeHeader";
+import { HomeThreeColumns } from "./HomeThreeColumns";
+import { HomeSimpleSteps } from "./HomeSimpleSteps";
+import { HomeAboutUs } from "./HomeAboutUs";
+import { HomeWhoWeHelp } from "./HomeWhoWeHelp";
+import { HomeContact } from "./HomeContact";
+
 export const Home = () => {
   //
   ////UI
