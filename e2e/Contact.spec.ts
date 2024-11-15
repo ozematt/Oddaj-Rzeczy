181225fd3c93dac3b66e2110c1015008f4d62db5
Mateusz Obirek <neutralnie7@gmail.com>
2024-11-15 14:25:49 +0100

    deleted test

--- e2e/Contact.spec.ts
+++ e2e/Contact.spec.ts
@@ -22,28 +22,7 @@ test.describe("Contact submit flow", () => {
     await page.getByRole("button", { name: "Wyślij" }).click();
     await expect(page.getByText("Wiadomość została wysłana!")).toBeVisible();
   });
-  test("should display error messages after submit contact form with empty fields ", async ({
-    page,
-  }) => {
-    await page.goto("/");
-    const contactButton = page
-      .getByRole("navigation")
-      .locator("li")
-      .filter({ hasText: "Kontakt" });
-    await contactButton.click();
 
-    await page.getByRole("button", { name: "Wyślij" }).click();
-
-    await expect(
-      page.getByText("Podane imię jest nieprawidłowe!")
-    ).toBeVisible();
-    await expect(
-      page.getByText("Podany email jest nieprawidłowy!")
-    ).toBeVisible();
-    await expect(
-      page.getByText("Wiadomość musi mieć conajmniej 120 znaków!")
-    ).toBeVisible();
-  });
   test("should display error messages after submit contact form with invalid data ", async ({
     page,
   }) => {
